b0bc2c042339e78c963bf2cf99fb0ba5d29b6eee
Elliott Kobelansky <elliottkobelansky@Elliotts-MacBook-Air.local>
2022-07-26 11:26:10 -0400

    Style and add next/last scramble functionality

--- js/main.js
+++ js/main.js
@@ -1,11 +1,55 @@
-let b = new bufferTrainer("UF");
-console.log(b.getScram());
+let bt = new bufferTrainer("UF");
 
-document.getElementById("genScram").onclick = gen;
+let nextscram = bt.getScram();
+let thisscram = "";
+let lastscram = "";
 
-let nextscram = b.getScram();
-function gen() {
-    console.log(nextscram);
-    nextscram = b.getScram();
+// Scramble generation. This can be improved by caching.
+function genScram() {
+    thisscram = nextscram;
+    nextscram = bt.getScram();
 }
 
+function displayScram() {
+    lastscram = thisscram;
+    document.getElementById("scramble").innerHTML = nextscram;
+    genScram();
+}
+
+function lastScram() {
+    document.getElementById("scramble").innerHTML = lastscram;
+}
+
+
+// Next scramble on space bar
+document.getElementById("next").onclick = function () {
+    displayScram();
+};
+document.addEventListener('keydown', event => {
+    if (event.code === 'Space') {
+        displayScram();
+    }
+})
+
+// Last scramble
+
+document.getElementById("last").onclick = function () {
+    lastScram();
+};
+
+// Selecting new buffer(s)
+let EDGEBUFFER = "UF"
+let CORNERBUFFER = "None"
+
+document.getElementById("edgebuffer").onchange = function () {
+    document.getElementById("edgebuffer").blur();
+    let e = document.getElementById("edgebuffer");
+    let value = e.options[e.selectedIndex].value; 
+    bt = new bufferTrainer(value);
+    genScram();
+    displayScram();
+};
+
+document.getElementById("cornerbuffer").onchange = function () {
+    document.getElementById("cornerbuffer").blur();
+};
