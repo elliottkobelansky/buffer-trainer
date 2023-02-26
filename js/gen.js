ca4fe522e121088d98d990cc7d2d7e902270bced
Elliott Kobelansky <elliottkobelansky@Elliotts-Air.lan>
2023-02-26 09:43:58 -0500

    special request from graham

---
+++ js/gen.js
@@ -0,0 +1,124 @@
+let EDGEBUFFERORDER = ['UF', 'UB', 'UR', 'UL', 'FR', 'FL', 'DF', 'DB', 'DR', 'DL'];
+let CORNERBUFFERORDER = ['UFR', 'UFL', 'UBR', 'UBL', 'DFR', 'DFL'];
+
+let EDGEBUFFER = 'UF';
+let CORNERBUFFER = 'UFR';
+
+let EXCLUDEDEDGES = getExcludedEdges("UF");
+let EXCLUDEDCORNERS = getExcludedCorners("UFR");
+
+let bt = new bufferTrainer(EDGEBUFFER, CORNERBUFFER, EXCLUDEDEDGES, EXCLUDEDCORNERS);
+
+let nextscram = bt.getScram();
+let thisscram = "";
+let lastscram = "";
+
+function newBufferTrainer() {
+    EXCLUDEDEDGES = getExcludedEdges(EDGEBUFFER);
+    EXCLUDEDCORNERS = getExcludedCorners(CORNERBUFFER);
+    bt = new bufferTrainer(EDGEBUFFER, CORNERBUFFER, EXCLUDEDEDGES, EXCLUDEDCORNERS);
+    genScram();
+}
+
+function getExcludedEdges(buffer) {
+    if (buffer === "None") {
+        return "All"
+    }
+    let bufferIndex = EDGEBUFFERORDER.indexOf(buffer);
+    return EDGEBUFFERORDER.slice(0, bufferIndex);
+}
+
+function getExcludedCorners(buffer) {
+    if (buffer === "None") {
+        return "All"
+    }
+    let bufferIndex = CORNERBUFFERORDER.indexOf(buffer);
+    return CORNERBUFFERORDER.slice(0, bufferIndex);
+}
+
+// Scramble generation. 
+function genScram() {
+    thisscram = nextscram;
+    nextscram = bt.getScram();
+}
+
+
+// Selecting new buffer(s)
+document.getElementById("edgebuffer").onchange = function () {
+    document.getElementById("edgebuffer").blur();
+    let e = document.getElementById("edgebuffer");
+    let value = e.options[e.selectedIndex].value; 
+    EDGEBUFFER = value;
+    EXCLUDEDEDGES = getExcludedEdges(value);
+    newBufferTrainer();
+};
+
+document.getElementById("cornerbuffer").onchange = function () {
+    document.getElementById("cornerbuffer").blur();
+    let e = document.getElementById('cornerbuffer');
+    let value = e.options[e.selectedIndex].value;
+    CORNERBUFFER = value;
+    EXCLUDEDCORNERS = getExcludedCorners(value);
+    newBufferTrainer();
+};
+
+
+// Change Buffer Order
+new Sortable(edgebufferlist, {
+    ghostClass: 'sortable-ghost',
+    chosenClass: 'sortable-chosen',
+});
+
+new Sortable(cornerbufferlist, {
+    ghostClass: 'sortable-ghost',
+    chosenClass: 'sortable-chosen',
+});
+
+document.getElementById("confirm").onclick = function () {
+    EDGEBUFFERORDER = Array.from(document.querySelectorAll("#edgebufferlist > li > a")).map(function (e) {return e.innerHTML;});
+    CORNERBUFFERORDER = Array.from(document.querySelectorAll("#cornerbufferlist > li > a")).map(function (e) {return e.innerHTML;});
+
+    let ebufferlist = Array.from(document.querySelectorAll("#edgebuffer > option"));
+    for (let i = 0; i < EDGEBUFFERORDER.length - 1; i++) {
+        ebufferlist[i + 1].innerHTML = EDGEBUFFERORDER[i];
+        ebufferlist[i + 1].value = EDGEBUFFERORDER[i];
+    }
+
+    let cbufferlist = Array.from(document.querySelectorAll("#cornerbuffer > option"));
+    for (let i = 0; i < CORNERBUFFERORDER.length - 1; i++) {
+        cbufferlist[i + 1].innerHTML = CORNERBUFFERORDER[i];
+        cbufferlist[i + 1].value = CORNERBUFFERORDER[i];
+    }
+    
+
+    document.getElementById('edgebuffer').options[1].selected = true;
+    document.getElementById('cornerbuffer').options[1].selected = true;
+
+    let e = document.getElementById("edgebuffer");
+    let value = e.options[e.selectedIndex].value; 
+    EDGEBUFFER = value;
+    EXCLUDEDEDGES = getExcludedEdges(value);
+
+    let ea = document.getElementById('cornerbuffer');
+    let valuea = ea.options[ea.selectedIndex].value;
+    CORNERBUFFER = valuea;
+    EXCLUDEDCORNERS = getExcludedCorners(valuea);
+
+    newBufferTrainer();
+}
+
+const form = document.getElementById('input');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let number = form.elements['number'].value;
+    let scrs = []
+    for (let i = 0; i < number; i++) {
+        console.log(i);
+        genScram();
+        scrs.push(thisscram);
+    }
+    scrs = scrs.join("\n")
+    document.getElementById("output").innerHTML = scrs;
+
+})
